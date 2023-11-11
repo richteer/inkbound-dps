eb891e72e02cdbd40ba7745374c0b3969369e007
Eric Richter <richteer@lastprime.net>
2023-11-11 17:24:54 -0600

    overlay: remove original colors from table
    Signed-off-by: Eric Richter <richteer@lastprime.net>

--- parser-overlay/src/lib.rs
+++ parser-overlay/src/lib.rs
@@ -17,19 +17,12 @@ mod egui_overlay_impl;
 fn class_string_to_color(class_name: &str) -> egui::Color32 {
     // TODO: use a complete lookup table so this only needs to be updated in one place
     match class_name {
-        // "Magma Miner"  => egui::Color32::from_rgb(217, 30, 24),
         "Magma Miner"  => egui::Color32::from_rgb(196, 75, 0),
-        // "Mosscloak"    => egui::Color32::from_rgb(38, 194, 129),
         "Mosscloak"    => egui::Color32::from_rgb(0, 130, 13),
-        // "Clairvoyant"  => egui::Color32::from_rgb(1, 1, 122),
         "Clairvoyant"  => egui::Color32::from_rgb(130, 0, 0),
-        // "Weaver"       => egui::Color32::from_rgb(150, 54, 148),
         "Weaver"       => egui::Color32::from_rgb(173, 148, 0),
-        // "Obelisk"      => egui::Color32::from_rgb(103, 128, 159),
         "Obelisk"      => egui::Color32::from_rgb(128, 128, 128),
-        // "Star Captain" => egui::Color32::from_rgb(244, 196, 48),
         "Star Captain" => egui::Color32::from_rgb(0, 119, 140),
-        // TODO: Chainbreaker, Godseeker
         "Chainbreaker" => egui::Color32::from_rgb(107, 0, 52),
         "Godseeker" => egui::Color32::from_rgb(150, 131, 2),
         _ => egui::Color32::DARK_GRAY,
