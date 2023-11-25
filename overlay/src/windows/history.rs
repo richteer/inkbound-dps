925267969d66e318fb77e95b805c0331cf4419b0
Eric Richter <richteer@lastprime.net>
2023-11-25 16:58:06 -0600

    overlay/history: add stacked mode to history plot
    Signed-off-by: Eric Richter <richteer@lastprime.net>

--- overlay/src/windows/history.rs
+++ overlay/src/windows/history.rs
@@ -1,15 +1,70 @@
 use egui::Window;
 use egui_plot::{Plot, Bar, BarChart, AxisHints};
-use inkbound_parser::parser::{DataLog, PlayerStats};
+use inkbound_parser::parser::{DataLog, PlayerStats, DiveLog};
 
 use crate::{Overlay, class_string_to_color};
 
 use super::show_dive_selection_box;
 
+#[derive(Default, PartialEq)]
+pub enum HistoryMode {
+    #[default]
+    Split,
+    Stacked,
+}
+
+impl std::fmt::Display for HistoryMode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let out = match self {
+            HistoryMode::Split => "Split",
+            HistoryMode::Stacked => "Stacked",
+        };
+        f.write_str(out)
+    }
+}
+
 #[derive(Default)]
 pub struct HistoryState {
     pub dive: usize,
+    pub mode: HistoryMode,
 }
+
+
+#[inline]
+fn generate_split_bars(dive: &DiveLog) -> Vec<Bar> {
+    dive.combats.iter().rev().enumerate().map(|(combat_index, combat)| {
+        let mut players: Vec<PlayerStats> = combat.player_stats.player_stats.values().cloned().collect();
+        players.sort_by(|a,b| a.player_data.name.cmp(&b.player_data.name) );
+        players.iter().enumerate().map(|(pind, p)| {
+            let num_players = combat.player_stats.player_stats.len() as f64;
+            let width = 1.0 / num_players as f64;
+            let x_offset = ((pind as f64 + 0.5) as f64 * width) - 0.5;
+            Bar::new(combat_index as f64 + x_offset + 1.0, p.total_damage_dealt as f64)
+                .name(format!("{} {}", p.player_data.name, combat_index + 1))
+                .width(width as f64)
+                .fill(class_string_to_color(p.player_data.class.as_str()))
+        }).collect::<Vec<Bar>>()
+    }).flatten().collect()
+}
+
+#[inline]
+fn generate_stacked_bars(dive: &DiveLog) -> Vec<Bar> {
+    dive.combats.iter().rev().enumerate().map(|(combat_index, combat)| {
+        let mut players: Vec<PlayerStats> = combat.player_stats.player_stats.values().cloned().collect();
+        players.sort_by_key(|p| p.total_damage_dealt);
+        players.iter().scan(0, |state, p| {
+            *state += p.total_damage_dealt;
+            Some((*state,  p))
+        }).map(|(previous, p)| {
+            Bar::new(combat_index as f64 + 1.0, p.total_damage_dealt as f64)
+                .name(format!("{} {}", p.player_data.name, combat_index + 1))
+                .base_offset((previous - p.total_damage_dealt) as f64)
+                .width(1.0)
+                .fill(class_string_to_color(p.player_data.class.as_str()))
+        }).collect::<Vec<Bar>>()
+    }).flatten().collect()
+}
+
 
 pub fn draw_history_window(overlay: &mut Overlay, ctx: &egui::Context, datalog: &DataLog) {
     if !overlay.options.show_history {
@@ -18,6 +73,13 @@ pub fn draw_history_window(overlay: &mut Overlay, ctx: &egui::Context, datalog: 
     
     Window::new("History").show(ctx, |ui| {
         show_dive_selection_box(ui, &mut overlay.window_state.history.dive, datalog.dives.len());
+        egui::ComboBox::from_label("Mode")
+            .selected_text(overlay.window_state.history.mode.to_string())
+            .show_ui(ui, |ui| {
+                ui.selectable_value(&mut overlay.window_state.history.mode, HistoryMode::Split, "Split");
+                ui.selectable_value(&mut overlay.window_state.history.mode, HistoryMode::Stacked, "Stacked");
+            })
+            .response.on_hover_text("Select which mode to render the history plot\n\nSplit - Each player has their own vertical bar per combat\nStacked - Player damage bars are stacked on top of each other, with the total length of the bar representing total group damage.");
 
         ui.separator();
         
@@ -27,19 +89,10 @@ pub fn draw_history_window(overlay: &mut Overlay, ctx: &egui::Context, datalog: 
             return
         };
 
-        let bars: Vec<Bar> = dive.combats.iter().rev().enumerate().map(|(combat_index, combat)| {
-            let mut players: Vec<PlayerStats> = combat.player_stats.player_stats.values().cloned().collect();
-            players.sort_by(|a,b| a.player_data.name.cmp(&b.player_data.name) );
-            players.iter().enumerate().map(|(pind, p)| {
-                let num_players = combat.player_stats.player_stats.len() as f64; 
-                let width = 1.0 / num_players as f64;
-                let x_offset = ((pind as f64 + 0.5) as f64 * width) - 0.5;
-                Bar::new(combat_index as f64 + x_offset + 1.0, p.total_damage_dealt as f64)
-                    .name(format!("{} {}", p.player_data.name, combat_index + 1))
-                    .width(width as f64)
-                    .fill(class_string_to_color(p.player_data.class.as_str()))
-            }).collect::<Vec<Bar>>()
-        }).flatten().collect();
+        let bars = match overlay.window_state.history.mode {
+            HistoryMode::Split => generate_split_bars(dive),
+            HistoryMode::Stacked => generate_stacked_bars(dive),
+        };
 
         let chart = BarChart::new(bars);
 
