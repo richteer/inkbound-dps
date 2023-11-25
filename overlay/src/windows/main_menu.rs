c9c3e14708609b07c72483dee5a7c0b3d3598f07
Eric Richter <richteer@lastprime.net>
2023-11-25 12:18:14 -0600

    overlay/settings: add help text to each setting option
    Signed-off-by: Eric Richter <richteer@lastprime.net>

--- overlay/src/windows/main_menu.rs
+++ overlay/src/windows/main_menu.rs
@@ -8,26 +8,29 @@ pub fn main_menu(overlay: &mut Overlay, ctx: &egui::Context) {
         // .resizable(false)
         .show(ctx, |ui| {
             ui.heading("Dive Overlays");
-            ui.checkbox(&mut overlay.options.show_dive_group_damage, "Show Group Damage");
-            ui.checkbox(&mut overlay.options.show_dive_individual_damage, "Show Individual Damage");
+            ui.checkbox(&mut overlay.options.show_dive_group_damage, "Show Group Damage")
+                .on_hover_text("Enable a plot showing the overall damage per player in a particular dive.");
+            ui.checkbox(&mut overlay.options.show_dive_individual_damage, "Show Individual Damage")
+                .on_hover_text("Enable a plot showing the damage per skill for a player in a dive.");
             ui.separator();
             ui.heading("Combat Overlays");
-            ui.checkbox(&mut overlay.options.show_combat_group_damage, "Show Group Damage");
-            ui.checkbox(&mut overlay.options.show_combat_individual_damage, "Show Individual Damage");
+            ui.checkbox(&mut overlay.options.show_combat_group_damage, "Show Group Damage")
+                .on_hover_text("Enable a plot showing the overall damage per player for a single combat encounter.");
+            ui.checkbox(&mut overlay.options.show_combat_individual_damage, "Show Individual Damage")
+                .on_hover_text("Enable a plot showing the damage per skill for a player in a single combat encounter.");
 
             ui.separator();
             ui.horizontal(|ui| {
                 ui.label("Default Player Name");
                 ui.text_edit_singleline(&mut overlay.options.default_player_name);
-            });
-            ui.add(egui::Slider::new(&mut overlay.options.plot_font_size, 4.0..=32.0).text("Plot font size"));
+            }).response.on_hover_text("Enter a player name to set as the default if no player is selected in an individual damage window.\n\nYou probably want to set this to your in-game character name.");
+            ui.add(egui::Slider::new(&mut overlay.options.plot_font_size, 4.0..=32.0).text("Plot font size"))
+                .on_hover_text("Set the font size for the damage plot labels.");
 
             ui.separator();
             if ui.button("Close Overlay").clicked() {
                 overlay.closing = true;
             }
-            // ui.set_width(ui.available_width());
-            // ui.set_height(ui.available_height());
         }
     );
 }
