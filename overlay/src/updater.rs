000323baaced89c1ea66b623f3854a4e74bb8caf
Eric Richter <richteer@lastprime.net>
2023-12-28 11:13:12 -0600

    feat(overlay/updater): add full changelog viewer when clicking on version
    When there is an update, you can view only the most recent changes, but
    not the full list. Add an option to view the whole changelog at will by
    clicking on the version.
    
    Signed-off-by: Eric Richter <richteer@lastprime.net>

--- overlay/src/updater.rs
+++ overlay/src/updater.rs
@@ -5,6 +5,8 @@ use crate::Overlay;
 #[derive(Default)]
 pub struct UpdateState {
     cache: egui_commonmark::CommonMarkCache,
+    full_cache: egui_commonmark::CommonMarkCache,
+    show_full: bool,
 }
 
 pub fn updater_settings(ui: &mut egui::Ui, overlay: &mut Overlay) {
@@ -19,7 +21,7 @@ pub fn updater_settings(ui: &mut egui::Ui, overlay: &mut Overlay) {
                 if ui.button("Check for update").clicked() {
                     updater.fetch_update(false);
                 }
-                ui.label(format!("v{}", updater.current_version));
+                version_label(ui, overlay, format!("v{}", updater.current_version));
             });
         },
         UpdateStatus::Fetching => {
@@ -34,10 +36,11 @@ pub fn updater_settings(ui: &mut egui::Ui, overlay: &mut Overlay) {
                     updater.do_update(false);
                 }
                 let current_version = updater.current_version.clone();
+                // TODO: consider adding full changelog here too?
                 let new_version = ui.colored_label(egui::Color32::GREEN, format!("New version available! {current_version} ➡ {version}"));
                 if let Some(body) = body {
                     new_version.on_hover_ui(|ui| {
-                        egui_commonmark::CommonMarkViewer::new("test").show(ui, &mut overlay.window_state.update.cache, &body);
+                        egui_commonmark::CommonMarkViewer::new("HoverChangelog").show(ui, &mut overlay.window_state.update.cache, &body);
                     });
                 }
             });
@@ -48,7 +51,7 @@ pub fn updater_settings(ui: &mut egui::Ui, overlay: &mut Overlay) {
                 if ui.button("Check for update").clicked() {
                     updater.fetch_update(false);
                 }
-                ui.label(format!("v{} - Up to date.", updater.current_version));
+                version_label(ui, overlay, format!("v{} - Up to date.", updater.current_version));
             });
         },
         UpdateStatus::Updating => {
@@ -60,4 +63,24 @@ pub fn updater_settings(ui: &mut egui::Ui, overlay: &mut Overlay) {
         UpdateStatus::Updated => { ui.colored_label(egui::Color32::GREEN, "Updated! Restart to use new version."); },
         UpdateStatus::Error(error) => { ui.colored_label(egui::Color32::RED, format!("Error updating: {error}")); },
     };
+
+    if overlay.window_state.update.show_full {
+        show_full_changelog(ui, overlay);
+    }
+}
+
+fn version_label(ui: &mut egui::Ui, overlay: &mut Overlay, label: String) {
+    let version = egui::Label::new(label).sense(egui::Sense::click());
+    let version = ui.add(version).on_hover_text("Click to show changelog");
+    if version.clicked() {
+        overlay.window_state.update.show_full = true;
+    }
+}
+
+fn show_full_changelog(ui: &mut egui::Ui, overlay: &mut Overlay) {
+    egui::Window::new("Changelog").open(&mut overlay.window_state.update.show_full).show(ui.ctx(), |ui| {
+        egui::ScrollArea::vertical().show(ui, |ui| {
+            egui_commonmark::CommonMarkViewer::new("FullChangelog").show(ui, &mut overlay.window_state.update.full_cache, include_str!("../../CHANGELOG.md"));
+        });
+    });
 }
